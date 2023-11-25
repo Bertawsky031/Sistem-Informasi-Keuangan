1ac4276670350a5c5aa4767cb8fdcf24ee06f4f5
Bertawsky031 <bertawsky02@gmail.com>
2023-11-25 15:35:36 +0700

    pertama commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright Bertawsky &#169; <script type='text/javascript'>var creditsyear = new Date();document.write(creditsyear.getFullYear());</script> <a expr:href='data:blog.homepageUrl'><data:blog.title/></a>. All rights reserved</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
